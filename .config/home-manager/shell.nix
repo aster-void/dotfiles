5c4f5466c7b487e24e62a4193a9dd9945e09e865
aster <137767097+aster-void@users.noreply.github.com>
2024-04-26 16:45:04 +0900

    changed some waybar

--- .config/home-manager/shell.nix
+++ .config/home-manager/shell.nix
@@ -1,6 +1,5 @@
 {
   programs.bash.shellAliases = {
-    hman = "cd ~/.config/home-manager; ls";
     hm = "home-manager";
     hb = "home-manager build";
     hs = "home-manager switch";
--- .config/home-manager/waybar.nix
+++ .config/home-manager/waybar.nix
@@ -1,31 +1,10 @@
-{config, pkgs, lib, ...}: 
+{ config, pkgs, lib, ... }:
 {
   programs.waybar = {
     enable = true;
     systemd.enable = true;
-    style = ''
-      ${builtins.readFile "${pkgs.waybar}/etc/xdg/waybar/style.css"}
-
-      * {
-        font-family: "MesloLGL Nerd Font";
-        border-radius: 10px;
-        margin: 0px 5px;
-        background-color: #31363F;
-        color: #76ABAE;
-      }
-      #pulseaudio {
-        background-color: #76ABAE;
-      }
-      #clock {
-      }
-      
-      window#waybar {
-        border-bottom: none;
-        background-color: rgba(34, 40, 49, 0.5);
-        color: rgb(34, 40, 49);
-      }
-
-    '';
+    /* ${builtins.readFile "${pkgs.waybar}/etc/xdg/waybar/style.css"} */
+    style = builtins.readFile "/home/aster/.dotfiles/.config/home-manager/waybar.style.css";
     settings = [{
       height = 30;
       layer = "top";
@@ -43,7 +22,7 @@
       battery = {
         format = "{capacity}% {icon}";
         format-alt = "{time} {icon}";
-        format-charging = "{capacity}% ";
+        format-charging = "{capacity}% 󰂄";
         format-icons = [ "" "" "" "" "" ];
         format-plugged = "{capacity}% ";
         states = {
@@ -52,9 +31,23 @@
         };
       };
       clock = {
-        format = "{:%Y-%m-%d | %H:%M}";
+        format = "{:%Y-%m-%d %H:%M}";
         alt = false;
-        tooltip-format = "{:%Y-%m-%d | %H:%M}";
+        tooltip-format = "<tt><small>{calendar}</small></tt>";
+        calendar = {
+          mode = "month";
+          on-scroll = 1;
+          on-click-right = "mode";
+          format = {
+            months = "<span color='#ffead3'><b>{}</b></span>";
+            days = "<span color='#ecc6d9'><b>{}</b></span>";
+            weekdays = "<span color='#ffcc66'><b>{}</b></span>";
+            today = "<span color='#ff6699'><b><u>{}</u></b></span>";
+          };
+        };
+        actions = {
+          on-click-right = "mode";
+        };
       };
       cpu = {
         format = "{usage}% ";
@@ -63,7 +56,7 @@
       memory = { format = "{}% "; };
       network = {
         interval = 1;
-        format-alt = " {bandwidthUpBits}  {bandwidthDownBits}";
+        tooltip-format = " {bandwidthUpBits}  {bandwidthDownBits}";
         format-disconnected = "Disconnected ⚠";
         format-ethernet = "{ifname}: {ipaddr}/{cidr} 󰈁";
         format-linked = "{ifname} (No IP) 󰌷";
@@ -74,8 +67,8 @@
         format-bluetooth = "{volume}% {icon} {format_source}";
         format-bluetooth-muted = " {icon} {format_source}";
         format-icons = {
-          car = "";
           default = [ "" "" "" ];
+          car = "";
           handsfree = "";
           headphones = "";
           headset = "";
@@ -95,4 +88,207 @@
       };
     }];
   };
+  /* default config as jsonc:
+  
+        // "layer": "top", // Waybar at top layer
+        // "position": "bottom", // Waybar position (top|bottom|left|right)
+        "height": 30, // Waybar height (to be removed for auto height)
+        // "width": 1280, // Waybar width
+        "spacing": 4, // Gaps between modules (4px)
+        // Choose the order of the modules
+        "modules-left": [
+        "sway/workspaces",
+        "sway/mode",
+        "sway/scratchpad",
+        "custom/media"
+        ],
+        "modules-center": [
+        "sway/window"
+        ],
+        "modules-right": [
+        "mpd",
+        "idle_inhibitor",
+        "pulseaudio",
+        "network",
+        "power-profiles-daemon",
+        "cpu",
+        "memory",
+        "temperature",
+        "backlight",
+        "keyboard-state",
+        "sway/language",
+        "battery",
+        "battery#bat2",
+        "clock",
+        "tray"
+        ],
+        // Modules configuration
+        // "sway/workspaces": {
+        //     "disable-scroll": true,
+        //     "all-outputs": true,
+        //     "warp-on-scroll": false,
+        //     "format": "{name}: {icon}",
+        //     "format-icons": {
+        //         "1": "",
+        //         "2": "",
+        //         "3": "",
+        //         "4": "",
+        //         "5": "",
+        //         "urgent": "",
+        //         "focused": "",
+        //         "default": ""
+        //     }
+        // },
+        "keyboard-state": {
+        "numlock": true,
+        "capslock": true,
+        "format": "{name} {icon}",
+        "format-icons": {
+            "locked": "",
+            "unlocked": ""
+        }
+        },
+        "sway/mode": {
+        "format": "<span style=\"italic\">{}</span>"
+        },
+        "sway/scratchpad": {
+        "format": "{icon} {count}",
+        "show-empty": false,
+        "format-icons": ["", ""],
+        "tooltip": true,
+        "tooltip-format": "{app}: {title}"
+        },
+        "mpd": {
+        "format": "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime:%M:%S}/{totalTime:%M:%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ",
+        "format-disconnected": "Disconnected ",
+        "format-stopped": "{consumeIcon}{randomIcon}{repeatIcon}{singleIcon}Stopped ",
+        "unknown-tag": "N/A",
+        "interval": 5,
+        "consume-icons": {
+            "on": " "
+        },
+        "random-icons": {
+            "off": "<span color=\"#f53c3c\"></span> ",
+            "on": " "
+        },
+        "repeat-icons": {
+            "on": " "
+        },
+        "single-icons": {
+            "on": "1 "
+        },
+        "state-icons": {
+            "paused": "",
+            "playing": ""
+        },
+        "tooltip-format": "MPD (connected)",
+        "tooltip-format-disconnected": "MPD (disconnected)"
+        },
+        "idle_inhibitor": {
+        "format": "{icon}",
+        "format-icons": {
+            "activated": "",
+            "deactivated": ""
+        }
+        },
+        "tray": {
+        // "icon-size": 21,
+        "spacing": 10
+        },
+        "clock": {
+        // "timezone": "America/New_York",
+        "tooltip-format": "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>",
+        "format-alt": "{:%Y-%m-%d}"
+        },
+        "cpu": {
+        "format": "{usage}% ",
+        "tooltip": false
+        },
+        "memory": {
+        "format": "{}% "
+        },
+        "temperature": {
+        // "thermal-zone": 2,
+        // "hwmon-path": "/sys/class/hwmon/hwmon2/temp1_input",
+        "critical-threshold": 80,
+        // "format-critical": "{temperatureC}°C {icon}",
+        "format": "{temperatureC}°C {icon}",
+        "format-icons": ["", "", ""]
+        },
+        "backlight": {
+        // "device": "acpi_video1",
+        "format": "{percent}% {icon}",
+        "format-icons": ["", "", "", "", "", "", "", "", ""]
+        },
+        "battery": {
+        "states": {
+            // "good": 95,
+            "warning": 30,
+            "critical": 15
+        },
+        "format": "{capacity}% {icon}",
+        "format-full": "{capacity}% {icon}",
+        "format-charging": "{capacity}% ",
+        "format-plugged": "{capacity}% ",
+        "format-alt": "{time} {icon}",
+        // "format-good": "", // An empty format will hide the module
+        // "format-full": "",
+        "format-icons": ["", "", "", "", ""]
+        },
+        "battery#bat2": {
+        "bat": "BAT2"
+        },
+        "power-profiles-daemon": {
+      "format": "{icon}",
+      "tooltip-format": "Power profile: {profile}\nDriver: {driver}",
+      "tooltip": true,
+      "format-icons": {
+        "default": "",
+        "performance": "",
+        "balanced": "",
+        "power-saver": ""
+      }
+        },
+        "network": {
+        // "interface": "wlp2*", // (Optional) To force the use of this interface
+        "format-wifi": "{essid} ({signalStrength}%) ",
+        "format-ethernet": "{ipaddr}/{cidr} ",
+        "tooltip-format": "{ifname} via {gwaddr} ",
+        "format-linked": "{ifname} (No IP) ",
+        "format-disconnected": "Disconnected ⚠",
+        "format-alt": "{ifname}: {ipaddr}/{cidr}"
+        },
+        "pulseaudio": {
+        // "scroll-step": 1, // %, can be a float
+        "format": "{volume}% {icon} {format_source}",
+        "format-bluetooth": "{volume}% {icon} {format_source}",
+        "format-bluetooth-muted": " {icon} {format_source}",
+        "format-muted": " {format_source}",
+        "format-source": "{volume}% ",
+        "format-source-muted": "",
+        "format-icons": {
+            "headphone": "",
+            "hands-free": "",
+            "headset": "",
+            "phone": "",
+            "portable": "",
+            "car": "",
+            "default": ["", "", ""]
+        },
+        "on-click": "pavucontrol"
+        },
+        "custom/media": {
+        "format": "{icon} {}",
+        "return-type": "json",
+        "max-length": 40,
+        "format-icons": {
+            "spotify": "",
+            "default": "🎜"
+        },
+        "escape": true,
+        "exec": "$HOME/.config/waybar/mediaplayer.py 2> /dev/null" // Script in resources folder
+        // "exec": "$HOME/.config/waybar/mediaplayer.py --player spotify 2> /dev/null" // Filter player based on name
+        }
+        }
+        */
 }
