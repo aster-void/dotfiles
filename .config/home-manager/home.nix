4975b64bad85bdd478f5ea40be3badee41f5b225
aster <137767097+aster-void@users.noreply.github.com>
2024-06-03 18:05:37 +0900

    added hyprcursor and tearing, removed sl

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -44,7 +44,6 @@
     waybar
 
     lolcat
-    sl
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
