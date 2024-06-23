41b5f1184f7983e177c7b05569b1c62a204d83cf
aster <137767097+aster-void@users.noreply.github.com>
2024-06-23 17:52:03 +0900

    fixed alacritty/init.sh and gitignore

--- .config/alacritty/init.sh
+++ .config/alacritty/init.sh
@@ -1,3 +1,3 @@
 cd $(dirname -- $0)
 
-git clone https://github.com/alacritty/alacritty-theme.git ${dir}/.config/alacritty/themes
+git clone https://github.com/alacritty/alacritty-theme.git ./themes
