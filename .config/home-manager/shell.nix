bb059b1c205074e500715ddb7413db071eacce7d
aster <137767097+aster-void@users.noreply.github.com>
2024-08-28 02:29:14 +0900

    fix zoxide not working

--- .config/home-manager/shell.nix
+++ .config/home-manager/shell.nix
@@ -8,10 +8,10 @@
       ccp = "cargo compete";
     };
 
-    # this doesn't seem to be working...
     bashrcExtra = ''
-      eval $(zoxide init bash)
+      source ~/.dotfiles/.zoxide.sh
       eval $(starship init bash)
+
     '';
     # don't forget to run this periodically: stow ~/.dotfiles
   };
