de056a846fd883acd55124ea1c5f95b2c56dc7d9
aster <137767097+aster-void@users.noreply.github.com>
2024-09-14 00:58:44 +0900

    move many packages from nixos

--- .config/home-manager/packages.nix
+++ .config/home-manager/packages.nix
@@ -2,24 +2,49 @@
 let
   self-hosting = import ./build/import.nix { inherit lib pkgs; };
   downloaded = with pkgs;  [
-    firefox
+    # Development
     nodejs_22
     vscode
+    act
+    postgresql
+
+    # GUI Applications
+
+    ## Connect
     slack
+    discord
+
+    ## Browsers
+    firefox
     chromium
-    zoom-us
+    brave
+    # firefox-devedition
+
+    ## Office
+    libreoffice
+    evince # document viewer
+
+    # CLI Applications
+    ## Music
+    cmus
+    cava
     yt-dlp
+    lollypop
+
+    ## .*fetch
     screenfetch
     neofetch
-    discord
-    steam
+
+    # Desktop Enhancement
+    waybar
+
+
+    ##
     # # overrides. You can do that directly here, just don't forget the
     # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
     # # fonts?
     # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-    waybar
 
-    lolcat
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
