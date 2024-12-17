77cf6ce741e0d9a5e9e24c5fbb742be2bfbcfd70
aster <137767097+aster-void@users.noreply.github.com>
2024-12-17 17:05:55 +0900

    update alacritty config and init script

--- init.sh
+++ init.sh
@@ -1,11 +1,22 @@
-# if [ ! $(nix-channel --list | grep "home-manager" | wc -l) == 0 ]; then
-  # echo "skipping home manager initialization"
-  # exit 0
-# fi
-nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager && \
-nix-channel --update && \
+#!/usr/bin/env bash
 
-./.config/home-manager/init.sh
+cd $(dirname -- $0)
 
-nix-shell -p stow --run 'stow .'
-nix-shell -p home-manager --run 'home-manager switch'
+# Home Manager Initialization
+if [ ! $(nix-channel --list | grep "home-manager" | wc -l) == 0 ]; then
+  echo "skipping home manager initialization"
+else
+  nix-channel --add https://github.com/nix-community/home-manager/archive/master.tar.gz home-manager && \
+  nix-channel --update && \
+
+  ./.config/home-manager/init.sh
+
+  nix-shell -p stow --run 'stow .'
+  nix-shell -p home-manager --run 'home-manager switch'
+fi
+
+# Initialize others
+./.config/alacritty/init.sh
+
+# reload stow
+stow .
