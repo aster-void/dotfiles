411166f1610fab0ee5eae64f3a81d82ea30c756a
aster <137767097+aster-void@users.noreply.github.com>
2024-12-17 15:27:37 +0900

    init vscode

--- .config/home-manager/apps/vscode.nix
+++ .config/home-manager/apps/vscode.nix
@@ -1,4 +1,14 @@
 # todo
 { pkgs, ... }: {
-  home.packages = [ pkgs.vscode ];
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+      mechatroner.rainbow-csv
+      oderwat.indent-rainbow
+      yoavbls.pretty-ts-errors
+      svelte.svelte-vscode
+      jnoortheen.nix-ide
+    ];
+  };
 }
