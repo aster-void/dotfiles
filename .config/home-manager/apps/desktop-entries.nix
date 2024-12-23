ffc08bb6156d791798ca0eda35691c63c641252a
aster <137767097+aster-void@users.noreply.github.com>
2024-12-23 19:26:57 +0900

    remove 'Wayland' from tail of app names

--- .config/home-manager/apps/desktop-entries.nix
+++ .config/home-manager/apps/desktop-entries.nix
@@ -1,12 +1,12 @@
 { pkgs, ... }: {
   xdg.desktopEntries = {
     slack = {
-      name = "Slack Wayland";
+      name = "Slack";
       exec = "slack --enable-wayland-ime %U";
       icon = "${pkgs.slack}/share/pixmaps/slack.png";
     };
     discord = {
-      name = "Discord Wayland";
+      name = "Discord";
       exec = "discord --enable-wayland-ime %U";
       icon = "${pkgs.discord}/share/pixmaps/discord.png";
     };
