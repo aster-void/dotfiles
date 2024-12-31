74306b8788b311f04c440522755a41ee9a4881df
aster <137767097+aster-void@users.noreply.github.com>
2025-01-01 01:19:51 +0900

    delete update.sh which was outdated. use `nix flake update` instead

--- update.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-cd `dirname -- $0`
-
-echo "updating home manager..."
-nix-channel --update 2>&1 > /dev/null
-home-manager switch 2>&1 > /dev/null
-echo "updating home manager done"
