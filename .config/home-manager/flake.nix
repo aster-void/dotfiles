be37c398a4b3dd68371cc01c7bbda3b0f24a1849
aster <137767097+aster-void@users.noreply.github.com>
2025-01-07 12:07:28 +0900

    allow helix from cachix

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -47,5 +47,10 @@
           inherit inputs username system;
         };
       };
+
+      nixConfig = {
+        extra-substituters = [ "https://helix.cachix.org" ];
+        extra-trusted-public-keys = [ "helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs=" ];
+      };
     };
 }
