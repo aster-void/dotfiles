ae1343793920e9fe90cbd51390369700f74ff76c
aster <137767097+aster-void@users.noreply.github.com>
2025-01-07 12:10:56 +0900

    fix init.sh and create hyprpaper sample

--- .config/hypr/init.sh
+++ .config/hypr/init.sh
@@ -2,15 +2,14 @@ cd $(dirname -- $0)
 
 (
   cd hyprland;
-
   if [ ! -f hardware-dep.conf ]; then
     cp samples/hardware-dep.conf hardware-dep.conf
   fi
   if [ ! -f plugins.conf ]; then
     cp samples/plugins.conf plugins.conf
   fi
-  if [ ! -f hyprpaper.conf ]; then
-    cp hyprpaper.conf.sample hyprpaper.conf
-  fi
-
 )
+
+if [ ! -f hyprpaper.conf ]; then
+  cp samples/hyprpaper.conf hyprpaper.conf
+fi
