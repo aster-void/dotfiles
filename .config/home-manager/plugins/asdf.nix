ebfd6c62e77ff14a7d77b85a7e0f92789342f535
aster <137767097+aster-void@users.noreply.github.com>
2025-01-18 20:44:08 +0900

    hm/asdf: add fish and zsh integration

--- .config/home-manager/plugins/asdf.nix
+++ .config/home-manager/plugins/asdf.nix
@@ -5,6 +5,13 @@
   ...
 }: let
   cfg = config.programs.asdf;
+  mkIntegrationOption = name:
+    lib.mkOption {
+      type = lib.types.bool;
+      description = "Whether to enable ${name} integration";
+      default = true;
+      example = false;
+    };
 in {
   options.programs.asdf = {
     enable = lib.mkEnableOption "asdf version manager";
@@ -12,11 +19,26 @@ in {
       type = lib.types.package;
       default = pkgs.asdf-vm;
     };
+    enableBashIntegration = mkIntegrationOption "bash";
+    enableZshIntegration = mkIntegrationOption "zsh";
+    enableFishIntegration = mkIntegrationOption "fish";
   };
 
   config = lib.mkIf cfg.enable {
-    programs.bash.initExtra = ''
-      . "${cfg.package}/share/asdf-vm/asdf.sh"
-    '';
+    # NOTE: not adding asdf-vm to home.packages because asdf seems to download its own bin under /share
+
+    programs = {
+      bash.initExtra = lib.optionalString cfg.enableBashIntegration ''
+        source "${cfg.package}/share/asdf-vm/asdf.sh"
+      '';
+
+      zsh.initExtra = lib.optionalString cfg.enableZshIntegration ''
+        source "${cfg.package}/share/asdf-vm/asdf.sh"
+      '';
+
+      fish.shellInitLast = lib.optionalString cfg.enableFishIntegration ''
+        source "${cfg.package}/share/asdf-vm/asdf.fish"
+      '';
+    };
   };
 }
