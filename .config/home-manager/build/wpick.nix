ec4dab2fadf0fca1948157ce7b45dd80007ce108
aster <137767097+aster-void@users.noreply.github.com>
2025-01-21 22:25:39 +0900

    home/wpick: add help

--- .config/home-manager/build/wpick.nix
+++ .config/home-manager/build/wpick.nix
@@ -8,11 +8,24 @@ pkgs.writeShellApplication {
   text = ''
     path=$(yazi --chooser-file=/dev/stdout)
     case $1 in
+      h|help|-h|--help)
+        echo "
+          usage:
+            wpick # pick your wallpaper
+            wpick lock # pick your lock screen
+            wpick all # choose both your wallpaper and wall
+            wpick help # show this help
+        ";
+      ;;
       l|lock)
         setpaper --lock "$path"
       ;;
       a|all)
         setpaper --wall --lock "$path"
+      ;;
+      -*)
+        echo "wpick: Unknown flag: $1"
+        exit 1
       ;;
       *)
         setpaper --wall "$path"
