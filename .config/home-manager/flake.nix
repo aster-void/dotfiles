fe5e31a0c653bae33dc7d36163ddc93e83d86ebb
aster <137767097+aster-void@users.noreply.github.com>
2025-02-11 16:44:13 +0900

    home: add helix to exra substituters

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -63,4 +63,8 @@
       };
     };
   };
+  nixConfig = {
+    extra-substituters = ["https://helix.cachix.org"];
+    extra-trusted-public-keys = ["helix.cachix.org-1:ejp9KQpR1FBI2onstMQ34yogDm4OgU2ru6lIwPvuCVs="];
+  };
 }
