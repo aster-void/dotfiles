8bdbdd541f406808b6c6b421e116fb5a674532c6
aster <137767097+aster-void@users.noreply.github.com>
2025-09-16 17:56:51 +0900

    hm: delete duplicate fcitx user service

--- home-manager/features/i18n/ime.nix
+++ home-manager/features/i18n/ime.nix
@@ -2,8 +2,6 @@
   fcitx5-addons = with pkgs; [
     fcitx5-mozc
     fcitx5-mozc-ut
-    mozcdic-ut-jawiki
-    mozcdic-ut-sudachidict
   ];
 in {
   i18n.inputMethod = {
@@ -16,24 +14,5 @@ in {
     GTK_IM_MODULE = "fcitx";
     QT_IM_MODULE = "fcitx";
     INPUT_METHOD = "fcitx";
-  };
-
-  systemd.user.startServices = "sd-switch";
-  systemd.user.services.fcitx5 = {
-    Unit = {
-      Description = "Fcitx5 input method";
-      PartOf = ["graphical-session.target"];
-      After = ["graphical-session.target"];
-    };
-    Service = {
-      Type = "dbus";
-      BusName = "org.fcitx.Fcitx5";
-      ExecStart = "${pkgs.bash}/bin/bash -c fcitx5";
-      Restart = "on-failure";
-      RestartSec = 1;
-    };
-    Install = {
-      WantedBy = ["graphical-session.target"];
-    };
   };
 }
