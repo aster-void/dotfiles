17c10feff4dd5778d5dbf9d7393c0d4fce11cd40
aster <137767097+aster-void@users.noreply.github.com>
2025-11-01 01:13:56 +0900

    flake: update inputs

--- flake.nix
+++ flake.nix
@@ -2,13 +2,15 @@
   description = "Home Manager configuration of aster";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    zen-browser.url = "github:0xc000022070/zen-browser-flake";
-    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nix-repository.url = "github:aster-void/nix-repository";
+    nix-repository.inputs.nixpkgs.follows = "nixpkgs";
+
+    # patch / workaround inputs
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
 
     # NixOS-specific inputs
     agenix.url = "github:ryantm/agenix";
@@ -16,26 +18,19 @@
     nix-flatpak.url = "github:gmodena/nix-flatpak/?ref=latest";
     lanzaboote.url = "github:nix-community/lanzaboote/v0.4.2";
     # lanzaboote.inputs.nixpkgs.follows = "nixpkgs";
-    comin = {
-      url = "github:nlewo/comin";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-repository = {
-      url = "github:aster-void/nix-repository";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    caelestia-shell = {
-      url = "github:caelestia-dots/shell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    caelestia-cli = {
-      url = "github:caelestia-dots/cli";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    mcp-servers-nix = {
-      url = "github:natsukium/mcp-servers-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    lanzaboote.inputs.rust-overlay.follows = "rust-overlay";
+    comin.url = "github:nlewo/comin";
+    comin.inputs.nixpkgs.follows = "nixpkgs";
+
+    # HM-specific inputs
+    zen-browser.url = "github:0xc000022070/zen-browser-flake";
+    zen-browser.inputs.nixpkgs.follows = "nixpkgs";
+    caelestia-shell.url = "github:caelestia-dots/shell";
+    caelestia-shell .inputs.nixpkgs.follows = "nixpkgs";
+    caelestia-cli.url = "github:caelestia-dots/cli";
+    caelestia-cli.inputs.nixpkgs.follows = "nixpkgs";
+    mcp-servers-nix.url = "github:natsukium/mcp-servers-nix";
+    mcp-servers-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = {nixpkgs, ...} @ inputs: let
@@ -58,8 +53,8 @@
       shared = import ./shared;
     };
   in {
+    inherit inputs overlays;
     packages.${system} = import ./my/pkgs/default.nix {inherit inputs pkgs;};
-    inherit overlays;
     nixosModules = import ./my/nixosModules;
     devShells.${system}.default = pkgs.mkShell {
       name = "dotfiles";
@@ -75,7 +70,7 @@
 
         # domain
         nwg-look
-        inputs.agenix.packages.${pkgs.system}.default
+        inputs.agenix.packages.${toString pkgs.system}.default
       ];
       env = {
         RULES = "secrets/secrets.nix";
