ec125ab6f26dd3454056e64520dd377e8057d20b
aster <137767097+aster-void@users.noreply.github.com>
2025-11-11 17:25:32 +0900

    nix: loosen nix.conf perms

--- nixos/core/secrets.nix
+++ nixos/core/secrets.nix
@@ -5,7 +5,11 @@ in {
     "/home/aster/.ssh/id_ed25519"
     "/root/.ssh/id_ed25519"
   ];
-  age.secrets.nix-access-tokens-conf.file = ../../secrets/nix.conf.age;
+  age.secrets.nix-access-tokens-conf = {
+    file = ../../secrets/nix.conf.age;
+    group = "users";
+    mode = "600";
+  };
 
   nix.extraOptions = ''
     !include ${secrets.nix-access-tokens-conf.path}
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,6 +1,5 @@
 let
   inherit (import ../meta.nix) publicKeys;
 in {
-  "secrets/github/public-access-token.age".publicKeys = publicKeys;
   "secrets/nix.conf.age".publicKeys = publicKeys;
 }
