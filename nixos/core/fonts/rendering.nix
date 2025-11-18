3d1e11724d6983205285a83db126cabb6e4f3639
aster <137767097+aster-void@users.noreply.github.com>
2025-11-18 17:48:51 +0900

    fix build

--- nixos/core/fonts/rendering.nix
+++ nixos/core/fonts/rendering.nix
@@ -2,15 +2,11 @@
   fonts = {
     fontconfig = {
       antialias = true;
-      hinting.enable = false;
+      hinting.enable = true;
+      hinting.style = "slight";
+      hinting.autohint = false;
       subpixel.rgba = "rgb";
-      gamma = 1.0;
       subpixel.lcdfilter = "light";
-    };
-    freetype.properties = {
-      "cff:no-stem-darkening" = "0";
-      "autofitter:no-stem-darkening" = "0";
-      "autofitter:darkening-parameters" = "0 0 0 0";
     };
   };
 }
