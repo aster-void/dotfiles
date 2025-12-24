7a85c8ce717000c88f871a346a9a5cd3e6923f87
aster <137767097+aster-void@users.noreply.github.com>
2025-12-24 23:39:50 +0900

    modules/base: use kmscon for Japanese TTY support
    Standard Linux console uses PSF fonts limited to 256/512 glyphs,
    making CJK display impossible. Switch to kmscon with Sarasa Term J
    font for full Unicode support in virtual consoles.

--- modules/nixos/base/system/console.nix
+++ modules/nixos/base/system/console.nix
@@ -1,4 +1,17 @@
-{
-  console.useXkbConfig = true;
+{pkgs, ...}: {
   boot.kernelParams = ["consoleblank=300"];
+
+  # Use kmscon instead of standard console for full Unicode/CJK support
+  services.kmscon = {
+    enable = true;
+    hwRender = true;
+    useXkbConfig = true;
+    fonts = [
+      {
+        name = "Sarasa Term J";
+        package = pkgs.sarasa-gothic;
+      }
+    ];
+    extraConfig = "font-size=14";
+  };
 }
