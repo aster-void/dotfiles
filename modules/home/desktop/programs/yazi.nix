5241a2c172dc03a5ad3fbf4a540c390692282ebf
aster <137767097+aster-void@users.noreply.github.com>
2025-12-24 16:23:21 +0900

    modules/desktop: configure yazi preview settings

--- modules/home/desktop/programs/yazi.nix
+++ modules/home/desktop/programs/yazi.nix
@@ -4,6 +4,12 @@
     package = pkgs.yazi;
     enableFishIntegration = true;
     settings = {
+      preview = {
+        image_filter = "nearest";
+        max_width = 600;
+        max_height = 600;
+      };
+
       tasks = {
         image_bound = [65535 65535];
       };
