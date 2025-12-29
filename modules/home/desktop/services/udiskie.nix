456b2a7712d2763b388fb093c57abee752d792db
aster <137767097+aster-void@users.noreply.github.com>
2025-12-29 20:42:43 +0900

    modules/desktop: add ~/Mounts symlink to udiskie mount point

--- modules/home/desktop/services/udiskie.nix
+++ modules/home/desktop/services/udiskie.nix
@@ -1,8 +1,11 @@
-{
+{config, ...}: {
   services.udiskie = {
     enable = true;
     automount = true;
     notify = true;
     tray = "auto";
   };
+
+  home.file."Mounts".source =
+    config.lib.file.mkOutOfStoreSymlink "/run/media/${config.home.username}";
 }
