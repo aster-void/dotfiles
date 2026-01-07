8b48410487570da901223db492178a9f0f7991ac
aster <137767097+aster-void@users.noreply.github.com>
2026-01-07 16:16:49 +0900

    hosts/azalea: fix igc driver after resume

--- hosts/azalea/configuration.nix
+++ hosts/azalea/configuration.nix
@@ -2,6 +2,11 @@
   networking.hostName = "azalea";
   networking.interfaces.enp3s0.wakeOnLan.enable = true;
 
+  # Intel I225-V (igc) needs reload after resume
+  powerManagement.resumeCommands = ''
+    modprobe -r igc && modprobe igc
+  '';
+
   imports = [
     flake.nixosModules.base
     flake.nixosModules.profile-dev
