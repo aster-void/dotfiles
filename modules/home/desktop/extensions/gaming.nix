738889ccbe576468efe96a136b3e5e4a055cfd3c
aster <137767097+aster-void@users.noreply.github.com>
2026-01-15 23:47:36 +0900

    modules/desktop: migrate gaming flatpaks from launcher.moe to flathub

--- modules/home/desktop/extensions/gaming.nix
+++ modules/home/desktop/extensions/gaming.nix
@@ -17,28 +17,13 @@ in {
     ];
 
     # Gaming-specific flatpak packages (base config in ../flatpak.nix)
-    services.flatpak.remotes = [
-      {
-        name = "launcher.moe";
-        location = "https://gol.launcher.moe/gol.launcher.moe.flatpakrepo";
-      }
-    ];
     services.flatpak.packages = [
       "org.gnome.Platform//49"
       "org.freedesktop.Platform.GL.nvidia-590-44-01//1.4"
       "org.freedesktop.Platform.GL32.nvidia-590-44-01//1.4"
-      {
-        appId = "moe.launcher.the-honkers-railway-launcher";
-        origin = "launcher.moe";
-      }
-      {
-        appId = "moe.launcher.sleepy-launcher";
-        origin = "launcher.moe";
-      }
-      {
-        appId = "io.mrarm.mcpelauncher";
-        origin = "flathub";
-      }
+      "moe.launcher.the-honkers-railway-launcher"
+      "moe.launcher.sleepy-launcher"
+      "io.mrarm.mcpelauncher"
     ];
   };
 }
