92989c7b83439907aac74d44787f3ede8c888eb7
aster <137767097+aster-void@users.noreply.github.com>
2026-01-22 19:33:57 +0900

    flake: uncomment nixpkgs override to enable cache

--- flake.nix
+++ flake.nix
@@ -24,7 +24,7 @@
 
     nix-repository.url = "github:aster-void/nix-repository";
     nix-repository.inputs = {
-      nixpkgs.follows = "nixpkgs";
+      #  nixpkgs.follows = "nixpkgs";
       blueprint.follows = "blueprint";
     };
 
